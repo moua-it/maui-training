0be9c965e547c1dcf3e0daefd15d65f8535fd5da
moua-it <contato@moua.com.br>
2025-09-13 15:21:02 -0300

    chore: initial structure and chapters (ContactSyncApp per chapter)

---
+++ src/chapter-2.1/ContactSyncApp/ContactSyncApp/ViewModel/ContactViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContactSyncApp.ViewModel
+{
+    public partial class ContactViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int contactId;
+
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private DateTime birthday;
+
+        [ObservableProperty]
+        private string address;
+
+        [ObservableProperty]
+        private string city;
+
+        [ObservableProperty]
+        private string state;
+
+        [ObservableProperty]
+        private string phoneNumber;
+
+        [ObservableProperty]
+        private string email;
+    }
+}
