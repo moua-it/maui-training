4b32ed9b73d15ca3d52e353a91ca1b2f6da24078
moua-it <contato@moua.com.br>
2025-09-14 09:09:50 -0300

    feat(chapter-4.2): add complete ContactSyncApp source

---
+++ src/chapter-4.2/ContactSyncApp/ContactSyncApp/ViewModel/ContactViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ContactSyncApp.ViewModel
+{
+    public partial class ContactViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int contactId;
+
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private DateTime birthday;
+
+        [ObservableProperty]
+        private string address;
+
+        [ObservableProperty]
+        private string city;
+
+        [ObservableProperty]
+        private string state;
+
+        [ObservableProperty]
+        private string phoneNumber;
+
+        [ObservableProperty]
+        private string email;
+
+        [RelayCommand]
+        private void SaveContact()
+        {
+            Console.WriteLine($"Salvando contato: {Name}");
+        }
+
+        [RelayCommand]
+        private void DeleteContact()
+        {
+            Console.WriteLine($"Excluindo contato: {Name}");
+        }
+    }
+}
