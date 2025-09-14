c7a3e0911ef15a977187dd0e0bc7f38a76a3e581
moua-it <contato@moua.com.br>
2025-09-14 08:21:25 -0300

    feat(chapter-2.2): add complete ContactSyncApp source

---
+++ src/chapter-2.2/ContactSyncApp/ContactSyncApp/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using ContactSyncApp.ViewModel;
+using ContactSyncApp.View;
+
+namespace ContactSyncApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddTransient<ContactViewModel>();
+            builder.Services.AddTransient<ContactFormPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
