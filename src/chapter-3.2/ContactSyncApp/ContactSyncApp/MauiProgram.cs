38327dad3b3cc0dc44a2cbdb71248bae011b4085
moua-it <contato@moua.com.br>
2025-09-14 08:58:30 -0300

    feat(chapter-3.2): add complete ContactSyncApp source

---
+++ src/chapter-3.2/ContactSyncApp/ContactSyncApp/MauiProgram.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using ContactSyncApp.ViewModel;
+using ContactSyncApp.View;
+
+namespace ContactSyncApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
+                    handlers.AddHandler(typeof(DatePicker), typeof(Microsoft.Maui.Handlers.DatePickerHandler));
+                    handlers.AddHandler(typeof(TimePicker), typeof(Microsoft.Maui.Handlers.TimePickerHandler));
+
+                    Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+                    {
+#if IOS
+                    if (handler.PlatformView is UIKit.UITextField nativeDatePicker)
+                    {
+                        nativeDatePicker.BorderStyle = UIKit.UITextBorderStyle.None;
+                    }
+#endif
+
+#if ANDROID
+                        if (handler.PlatformView is Android.Widget.EditText nativeDatePicker)
+                        {
+                            nativeDatePicker.Background = null;
+                        }
+#endif
+                    });
+                });
+
+            builder.Services.AddTransient<ContactViewModel>();
+            builder.Services.AddTransient<ContactFormPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
